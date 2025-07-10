34033f45953a5a29fe3c17a4f6932f0685a8a1fa
Hiep Quach <quachkyhiepslslmers@gmail.com>
2025-07-10 16:17:42 +0700

    Test auto trigger

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "🚀 Hello from Flask CI/CD with Jenkins & Docker!"
+    return "🚀 Hello from Flask CI/CD with Jenkins & Docker Quách Kỳ Hiệp! "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
