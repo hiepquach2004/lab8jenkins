daf5ea9f09239086e3c05fa7ab9fac92ab2072ef
Hiep Quach <quachkyhiepslslmers@gmail.com>
2025-07-10 16:33:36 +0700

    test webhook trigger

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "🚀 Hello from Flask CI/CD with Jenkins & Docker Quách Kỳ Hiệp! 22DH111096"
+    return "🚀 Hello from Flask CI/CD with Jenkins & Docker Quách Kỳ Hiệp! 22DH111096!!!!!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
